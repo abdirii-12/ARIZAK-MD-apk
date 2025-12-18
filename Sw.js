abd103fa1dd46793c6ea4bb29aa80de0d79d69ee
abdirii-12 <adanabdirii833@gmail.com>
2025-12-18 20:46:17 +0300

    Remove existing service worker implementation

--- Sw.js
+++ Sw.js
@@ -1,17 +1,1 @@
-// SIMPLE SERVICE WORKER THAT WORKS
-self.addEventListener('install', function(event) {
-  console.log('Service Worker: Installed');
-  // Skip waiting so it activates immediately
-  self.skipWaiting();
-});
 
-self.addEventListener('activate', function(event) {
-  console.log('Service Worker: Activated');
-  // Claim clients immediately
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', function(event) {
-  // Just pass through - no caching issues
-  event.respondWith(fetch(event.request));
-});
